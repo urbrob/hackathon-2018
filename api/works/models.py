ea873d97dfa2ec496733ea4c216071e27392b3d3
urbrob <35544390+urbrob@users.noreply.github.com>
2018-12-01 11:26:52 +0100

    Add organization (#1)

--- api/works/models.py
+++ api/works/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(model.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    email_contact = models.CharField(max_length=30)
+    phone_contact = models.CharField(max_length=30)
