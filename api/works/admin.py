4a897df941443a7000887e46e28ea3c80911804a
DawidCwiek <cwiekdawid1@gmai.com>
2018-12-01 12:45:42 +0100

    admin.v2

--- api/works/admin.py
+++ api/works/admin.py
@@ -1,16 +1,32 @@
 from django.contrib import admin
-from works.models import Organization
+from works.models import Organization, Group, TasksList, Report
+
+
+class GroupInline(admin.TabularInline):
+    model = Group
+    extra = 0
 
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     search_fields = ['name', 'address', 'email_contact', 'phone_contact']
     fields = ('name', 'address', 'email_contact', 'phone_contact')
+    inlines = [
+        GroupInline,
+    ]
 
-"""@admin.register(Group)
+@admin.register(Group)
 class Group(admin.ModelAdmin):
     search_fields = ['name', 'teacher', 'organization']
     fields = ('name', 'teacher', 'organization')
     list_display = ('name', 'teacher', 'organization_name')
 
-@admin.register()"""
+@admin.register(TasksList)
+class TasksList(admin.ModelAdmin):
+    list_display = ('id', )
+
+@admin.register(Report)
+class Report(admin.ModelAdmin):
+    search_fields = ['task']
+    fields = ('task',)
+    list_display = ('task',)
