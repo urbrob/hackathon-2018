2f8d2280066292925c6468b06f514ed064cf7358
Adam Pęczkowski <klejton@MacBook-Pro-Adam.local>
2018-12-01 16:18:57 +0100

    modele prawie skonczone

--- api/works/models.py
+++ api/works/models.py
@@ -19,6 +19,9 @@ class User(AbstractUser):
         default=STUDENT,
     )
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.username}'
+
 
 class Organization(models.Model):
     name = models.CharField(max_length=50)
@@ -34,35 +37,51 @@ class GroupMembership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_teacher = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.user.username} - {self.is_teacher}'
+
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.organization.name}'
 
 
 class TasksList(models.Model):
     group = models.ManyToManyField(Group)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.group.name} - {self.organization.name}'
+
 
 class TaskAssign(models.Model):
     task_list = models.ForeignKey(TasksList, on_delete=models.CASCADE)
-    error = models.CharField(null=True, max_length=150)
-    status = models.CharField(max_length=30, choices=TASK_STATUSES)
-    line = models.IntegerField(null=True)
+    task = models.ForeignKey('Task', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.task_list.group.name} - {self.task.list.organization}'
 
 
 class Task(models.Model):
     tesk_list = models.ManyToManyField(TasksList, through='TaskAssign')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.tesk_list.group.name}'
 
 
 class Test(models.Model):
     task = models.ManyToManyField(Task)
 
 
+
 class TestResult(models.Model):
     IN_PROGRESS = 'in-progress'
-    FAILED = 'failed',
+    FAILED = 'failed'
     PASSED = 'passed'
     TASK_STATUSES = (
         (IN_PROGRESS, 'In progress'),
@@ -72,10 +91,19 @@ class TestResult(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
     report = models.ForeignKey('Report', on_delete=models.CASCADE)
     task = models.ForeignKey('Task', on_delete=models.CASCADE)
+    error = models.CharField(null=True, max_length=150)
+    status = models.CharField(max_length=30, choices=TASK_STATUSES)
+    line = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.task.title} {self.report.id} - {self.status}'
 
 
 class Report(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     accepted_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='reports', null=True)
-    tests_result = models.ManyToManyField(Test, through='TestResult')
     student = models.ForeignKey(User, on_delete=models.CASCADE)
+    teacher_comment = models.CharField(null=True, max_length=250)
+
+    def __str__(self):
+        return f'{self.taask.title} - {self.tests_result}'
