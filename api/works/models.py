8f110ca9269098b58a27937faba0b855e80e1818
Adam Pęczkowski <klejton@MacBook-Pro-Adam.local>
2018-12-02 04:42:29 +0100

    permissions poprawka kolejna

--- api/works/models.py
+++ api/works/models.py
@@ -29,19 +29,28 @@ class User(AbstractUser):
 
         if self.status == self.STUDENT:
             self.user_permissions.clear()
-            self.user_permissions.add(Permission.objects.get(codename='view_report'),
+            self.user_permissions.add(
+            Permission.objects.get(codename='view_report'),
             Permission.objects.get(codename='add_report'),
+            Permission.objects.get(codename='change_report'),
             Permission.objects.get(codename='view_testresult'),
+            Permission.objects.get(codename='change_testresult'),
             Permission.objects.get(codename='view_taskslist'),
+            Permission.objects.get(codename='change_taskslist'),
             Permission.objects.get(codename='view_task'),
-            Permission.objects.get(codename='view_test'))
+            Permission.objects.get(codename='change_task'),
+            Permission.objects.get(codename='view_test'),
+            Permission.objects.get(codename='change_test'))
 
         if self.status == self.TEACHER:
             self.user_permissions.clear()
-            self.user_permissions.add(Permission.objects.get(codename='view_users'),
+            self.user_permissions.add(
+            Permission.objects.get(codename='view_user'),
+            Permission.objects.get(codename='change_user'),
             Permission.objects.get(codename='view_report'),
             Permission.objects.get(codename='change_report'),
             Permission.objects.get(codename='view_testresult'),
+            Permission.objects.get(codename='change_testresult'),
             Permission.objects.get(codename='view_taskslist'),
             Permission.objects.get(codename='add_taskslist'),
             Permission.objects.get(codename='change_taskslist'),
@@ -59,9 +68,7 @@ class User(AbstractUser):
             Permission.objects.get(codename='delete_groupmembership'),
             Permission.objects.get(codename='change_groupmembership'),
             Permission.objects.get(codename='view_group', content_type__app_label='works'),
-            Permission.objects.get(codename='add_groupmembership'),
-            Permission.objects.get(codename='delete_groupmembership'),
-            Permission.objects.get(codename='change_groupmembership'))
+            Permission.objects.get(codename='change_group', content_type__app_label='works'))
 
         if self.status == self.ADMIN:
             self.user_permissions.clear()
